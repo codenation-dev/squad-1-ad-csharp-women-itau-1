d10ac33993cbbe4f96b4470af44db5138dd2d041
carlaferrarez <carlacristinef@gmail.com>
2020-04-22 22:07:31 -0300

    adição MODELS: evento

---
+++ ProjetoFinal/ProjetoFinal/Models/Context.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoFinal.Models
+{
+    public class Context : DbContext
+    {
+      
+        public DbSet<Evento> Evento { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Squad;Trusted_Connection=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}

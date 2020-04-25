65d6a5ece1808e582a363fa09ed7b57045772fe4
mandypry <amanda_sucesso@hotmail.com>
2020-04-25 16:06:22 -0300

    Correção de repositório

---
+++ ProjetoFinal/ProjetoFinal/Services/ErroService.cs
@@ -0,0 +1,22 @@
+﻿using ProjetoFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoFinal.Services
+{
+    public class ErroService
+    {
+        private Context _context;
+
+        public ErroService(Context context)
+        {
+            _context = context;
+        }
+
+        // public IList<Erro>
+
+
+    }
+}

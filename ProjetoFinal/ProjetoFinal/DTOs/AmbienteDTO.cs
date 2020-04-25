199bc8ffa3506fc34df66f9fa3dbb210d63aacd1
Aline de Lima Santos <linsantos93@gmail.com>
2020-04-25 15:00:47 -0300

    Ambiente Model/DTO/Service

---
+++ ProjetoFinal/ProjetoFinal/DTOs/AmbienteDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoFinal.DTOs
+{
+    public class AmbienteDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string NomeAmbiente { get; set; }
+    }
+}
---
+++ ProjetoFinal/ProjetoFinal/Services/AmbienteService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoFinal.Models;
+
+namespace ProjetoFinal.Services
+{
+    public class AmbienteService
+    {
+        private Context _context;
+
+        public AmbienteService(Context context)
+        {
+            _context = context;
+        }
+
+        public Ambiente ProcurarPorId(int id)
+        {
+            return _context.Ambientes.Find(id);
+        }
+
+        public IList<Ambiente> ListarAmbientes()
+        {
+            return _context.Ambientes.ToList();
+        }
+
+        public Ambiente Salvar(Ambiente ambiente)
+        {
+            var ambienteEncontrado = _context.Ambientes
+                                              .Where(x => x.Id == ambiente.Id)
+                                              .FirstOrDefault();
+
+            if (ambienteEncontrado == null)
+                _context.Add(ambiente);
+            else
+                ambienteEncontrado.NomeAmbiente = ambiente.NomeAmbiente;
+
+            _context.SaveChanges();
+            return ambiente;
+        }
+    }
+}

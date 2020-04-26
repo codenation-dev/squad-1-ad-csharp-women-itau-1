db8ee616f0370393b91b2d99ece45aa388d7a09a
carlaferrarez <carlacristinef@gmail.com>
2020-04-26 20:07:24 -0300

    Adicionando Automapper

---
+++ ProjetoFinal/ProjetoFinal/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ProjetoFinal.DTOs;
+using ProjetoFinal.Models;
+
+namespace ProjetoFinal
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Evento, EventoDTO>().ReverseMap();
+        }
+    }
+}

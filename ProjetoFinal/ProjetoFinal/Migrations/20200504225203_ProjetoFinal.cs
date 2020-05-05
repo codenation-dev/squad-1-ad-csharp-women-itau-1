0dc15d696cbcda103c88e87ccad1acee19dc9792
Aline de Lima Santos <linsantos93@gmail.com>
2020-05-04 23:47:18 -0300

    Finalização do POST de Erro, Controllers de Ambiente e Nivel e alterações gerais para Foreign Keys

---
+++ ProjetoFinal/ProjetoFinal/Migrations/20200504225203_ProjetoFinal.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjetoFinal.Migrations
+{
+    public partial class ProjetoFinal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

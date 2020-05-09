5d83e83f1f2e063b4f55a9b246803805e16d0131
Aline de Lima Santos <linsantos93@gmail.com>
2020-05-08 22:34:14 -0300

    Remoção de comentários

--- ProjetoFinal/ProjetoFinal/Services/EmailServices.cs
+++ ProjetoFinal/ProjetoFinal/Services/EmailServices.cs
@@ -22,10 +22,7 @@ namespace ProjetoFinal.Services
         {
             try
             {
-                // buscar SendGrid key
                 var client = new SendGridClient(_sendGridOptions.SendGridKey);
-
-                // obj de email
                 var msg = new SendGridMessage()
                 {
                     From = new EmailAddress(_sendGridOptions.FromEmail, _sendGridOptions.FromFullName),
@@ -34,17 +31,13 @@ namespace ProjetoFinal.Services
                     HtmlContent = mensagem
                 };
 
-                // email do usuario
                 msg.AddTo(new EmailAddress(email));
-
-                // envio do email
                 var responseSend = await client.SendEmailAsync(msg);
+                var retorno = new EmailResponse
+                {
+                    Enviado = true
+                };
 
-                // obj retorno
-                var retorno = new EmailResponse();
-                retorno.Enviado = true;
-
-                // verificação de envio
                 if (!responseSend.StatusCode.Equals(System.Net.HttpStatusCode.Accepted))
                 {
                     retorno.Enviado = false;
